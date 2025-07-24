490818ab9ac18c5922fb9468b0f103525dd34cba
ShyamBansal01 <137391020+ShyamBansal01@users.noreply.github.com>
2025-07-24 16:21:25 +0530

    Create Q5.cpp

---
+++ Q5.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n = 2520;
+    n *= (1LL*11*13*17*19*2);
+    //cout<<2*3*2*5*7*2*3;
+    cout<<n<<endl;
+}
