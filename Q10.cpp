7ecc161beb1fc156effb79255be00fd6d257b0c0
ShyamBansal01 <137391020+ShyamBansal01@users.noreply.github.com>
2025-07-29 18:39:14 +0530

    Create Q10.cpp

---
+++ Q10.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+//------------------------------------------------------------------------
+vector<bool> is_prime(2e6 + 3, 1);
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for(int i = 2; i <= 2e6; i++){
+        if(is_prime[i]){
+            for(int j = i*i; j <= 2e6; j += i){
+                is_prime[j] = false;
+            }
+        }
+    }
+    int a = 0;
+    for(int i = 0; i <= 2e6; i++){
+        if(is_prime[i]){
+            a+= i;
+        }
+    }
+    cout<<a<<endl;
+}
